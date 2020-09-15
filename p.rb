ca1d4f0cc802d3c632de9fdd01668d88b568c3a1
classy-tier-0049 <mauricelynell@gmail.com>
2020-09-15 14:32:19 +0000

    Automatically backed up by Learn

---
+++ p.rb
@@ -0,0 +1,31 @@
+# array_of_arrays = [ [1, 2, 3],
+#       [4, 5, 6],
+#       [7, 8, 9]
+#     ]
+    
+# count = 0
+
+# while count < array_of_arrays.length do
+#   p array_of_arrays[count]
+#   count2 = 0
+#   while count2<array_of_arrays[count].length do
+#     p array_of_arrays[count][count2]
+#     count2 += 1
+#   end
+#   count += 1
+# end
+
+
+array = ["apple", "banna"]
+
+
+def arr(a)
+  count = 0
+  while count < a.length do
+    if a[count][0] == "a"
+      puts a[count]
+    end
+    count +=1
+  end
+end
+arr(array)
